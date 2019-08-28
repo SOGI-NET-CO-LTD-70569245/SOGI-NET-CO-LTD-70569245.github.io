61053d59b6246f13a7375a0a4cf87b3a9dfef3a0
steking1207 <steking1207@gmail.com>
2019-08-28 09:27:22 +0800

    190828

--- assets/javascripts/function/star-rating.js
+++ assets/javascripts/function/star-rating.js
@@ -2,7 +2,7 @@ $(document).on('ready', function () {
     $('.kv-fa').rating({
         showCaption: false,
         theme: 'krajee-fa',
-        filledStar: '<i class="fa fa-star"></i>',
-        emptyStar: '<i class="fa fa-star-o"></i>'
+        filledStar: '<i class="fas fa-star"></i>',
+        emptyStar: '<i class="far fa-star"></i>'
     });
 });
