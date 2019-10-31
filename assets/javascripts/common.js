1b77f9d982225f1e907897510dda6feffd7c1009
steking1207 <steking1207@gmail.com>
2019-10-31 23:05:47 +0800

    191031

--- assets/javascripts/common.js
+++ assets/javascripts/common.js
@@ -208,9 +208,9 @@ $(function() {
     "use strict";
 
     $('.dropdown').hover(function() {
-        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+        $(this).find('.dropdown-menu').stop(true, true).fadeIn(200);
     }, function() {
-        $(this).find('.dropdown-menu').stop(true, true).delay(10000).fadeOut(200);
+        $(this).find('.dropdown-menu').stop(true, true).fadeOut(200);
     });
 
 //    $('body').click( function() {
