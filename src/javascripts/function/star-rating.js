bef89a90d411a9093331c3e747cbaaefd23df858
steking1207 <steking1207@gmail.com>
2022-03-24 00:12:49 +0800

    src/js

---
+++ src/javascripts/function/star-rating.js
@@ -0,0 +1,8 @@
+$(document).on('ready', function () {
+    $('.kv-fa').rating({
+        showCaption: false,
+        theme: 'krajee-fa',
+        filledStar: '<i class="fas fa-star"></i>',
+        emptyStar: '<i class="far fa-star"></i>'
+    });
+});
