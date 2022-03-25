9ef4a69e32b5070db927725362023a4fdf31cb58
steking1207 <steking1207@gmail.com>
2022-03-25 19:36:07 +0800

    add: original file backup / 原有前端檔案備份

---
+++ output/_bak/assets/javascripts/function/include.js
@@ -0,0 +1,45 @@
+// Common
+$("#common-header").load("/module/common-header.html");
+$("#common-footer").load("/module/common-footer.html");
+
+// Store
+$("#store-admin-header").load("/module/store-admin-header.html");
+$("#store-admin-footer").load("/module/store-admin-footer.html");
+$("#nav-mobile-store-header").load("/module/nav-mobile-store-header.html");
+$("#title-store").load("/module/title-store.html");
+$("#products-store-new").load("/module/products-store-new.html");
+$("#products-store-second").load("/module/products-store-second.html");
+$("#products-store-repair").load("/module/products-store-repair.html");
+$("#list-group-collapse").load("/module/list-group-collapse.html");
+$("#upload-images").load("/module/upload-images.html");
+
+// Components
+$("#nav-mobile-common-header").load("/module/nav-mobile-common-header.html");
+$("#nav-main").load("/module/nav-main.html");
+$("#com-breadcrumb").load("/module/com-breadcrumb.html");
+$(".com-pagination").load("/module/com-pagination.html");
+$(".forum-functions").load("/module/forum-functions.html");
+
+// Section
+$("#article-headlines").load("/module/article-headlines.html");
+$(".article-list-full").load("/module/article-list-full.html");
+$("#popup-message").load("/module/popup-message.html");
+
+// Model
+$("#modal-mge-public").load("/module/modal-mge-public.html");
+$("#modal-mge-tag").load("/module/modal-mge-tag.html");
+$("#modal-mge-best").load("/module/modal-mge-best.html");
+$("#modal-public").load("/module/modal-public.html");
+$("#modal-admin").load("/module/modal-admin.html");
+$("#modal-friend").load("/module/modal-friend.html");
+$("#modal-refresh").load("/module/modal-refresh.html");
+$("#modal-buy").load("/module/modal-buy.html");
+$("#modal-login").load("/module/modal-login.html");
+$("#modal-track").load("/module/modal-track.html");
+$("#modal-pk").load("/module/modal-pk.html");
+$("#modal-quickview").load("/module/modal-quickview.html");
+
+// AD
+$("#ad-header").load("/module/ad-header.html");
+$("#ad-sponsor").load("/module/ad-sponsor.html");
+$(".ad-double-300-250").load("/module/ad-double-300-250.html");
