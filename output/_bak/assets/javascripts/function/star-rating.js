a1068103575130ad6393c0b0651f4e827ee993af
steking1207 <steking1207@gmail.com>
2022-04-21 09:46:59 +0800

    remove: output

--- output/_bak/assets/javascripts/function/star-rating.js
+++
@@ -1,8 +0,0 @@
-$(document).on('ready', function () {
-    $('.kv-fa').rating({
-        showCaption: false,
-        theme: 'krajee-fa',
-        filledStar: '<i class="fas fa-star"></i>',
-        emptyStar: '<i class="far fa-star"></i>'
-    });
-});
