80042bb052b19097e4a55e3ae629bd857b90b0a9
steking1207 <steking1207@gmail.com>
2022-07-14 10:54:17 +0800

    Update 2022-7-14 10:54:12

--- assets/javascripts/function/countdown.js
+++ assets/javascripts/function/countdown.js
@@ -2,6 +2,7 @@ $('[data-countdown]').each(function() {
     var $this = $(this), finalDate = $(this).data('countdown');
     $this.countdown(finalDate, function(event) {
         $this.html(event.strftime(''
+        + '<span class="count-time">%D</span>:'
         + '<span class="count-time">%H</span>:'
         + '<span class="count-time">%M</span>:'
         + '<span class="count-time">%S</span>'));
