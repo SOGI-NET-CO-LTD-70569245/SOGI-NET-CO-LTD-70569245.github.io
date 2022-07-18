802b8c680da34392a3dec2508386e63a6855c5b6
steking1207 <steking1207@gmail.com>
2022-07-18 18:35:48 +0800

    fix: remove mititup

--- src/javascripts/function/countdown.js
+++ src/javascripts/function/countdown.js
@@ -2,7 +2,7 @@ $('[data-countdown]').each(function() {
     var $this = $(this), finalDate = $(this).data('countdown');
     $this.countdown(finalDate, function(event) {
         $this.html(event.strftime(''
-        + '<span class="count-time">%D</span>:'
+        + '<span class="count-time">%D</span>天'
         + '<span class="count-time">%H</span>:'
         + '<span class="count-time">%M</span>:'
         + '<span class="count-time">%S</span>'));
