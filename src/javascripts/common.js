9dbb98f5bdbff60634745712f321ff3bb432a8d8
steking1207 <steking1207@gmail.com>
2024-07-30 15:20:29 +0800

    fix: fb sdk

--- src/javascripts/common.js
+++ src/javascripts/common.js
@@ -282,13 +282,13 @@ $(document).ready(function(){
     // });
 });
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.5";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+// (function(d, s, id) {
+//   var js, fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s); js.id = id;
+//   js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.5";
+//   fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
 
 
 // 191026
